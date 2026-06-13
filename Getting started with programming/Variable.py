ad8d8f51564a25a3b467e47c97949f2481c7280e
Amay <amaysharma17@icloud.com>
2026-06-13 14:35:48 +0530

    Python

---
+++ Getting started with programming/Variable.py
@@ -0,0 +1,8 @@
+x = "Amay"
+y = 17
+print(x)
+print(y)
+amay = "amay"
+print(amay)
+name = input("Enter Your Name : ")
+print("Your name is : ", name )
---
+++ Getting started with programming/Welcome.py
@@ -0,0 +1,12 @@
+# Welcome to Programming
+print("Welcome to the world of programming!")
+# Print a number
+print(7)
+# New line after print 
+print("Hello Python \n")
+# Passing multiple arguments for printing
+print("Hello",17)
+# Use of end argument
+print("Hello Coding",end=' ** ')
+# Hello 
+print("Hello")
